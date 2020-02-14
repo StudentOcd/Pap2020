36c9ca5259473a1ac1b4eb302b86feb29369f997
João Fernandes <joao.fernandes.cb@gmail.com>
2020-02-14 11:20:56 +0000

    ROUTE CONFIG

--- Pap2020/App_Start/RouteConfig.cs
+++ Pap2020/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Pap2020
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Utilizadors", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
